57489319b18993708311ae6e4363f40122605fe6
Hadi Santoso <hadisnt15@gmail.com>
2022-02-06 18:09:19 +0800

    Membuat crud tabel jabatan dan bagian
    Hal 27-30

---
+++ 18630632_HadiSantoso/commit/27-30 Membuat crud tabel jabatan dan bagian/bagiandelete.php
@@ -0,0 +1,22 @@
+<?php 
+
+if (isset($_GET['id'])) {
+  $id = $_GET['id'];
+
+  $database = new Database();
+  $db = $database->getConnection();
+
+  $deleteSql = "DELETE FROM bagian WHERE id = ?";
+  $stmt = $db->prepare($deleteSql);
+  $stmt->bindParam(1, $_GET['id']);
+  if ($stmt->execute()) {
+    $_SESSION['hasil'] = true;
+    $_SESSION['pesan'] = "Berhasil Hapus Data Bagian";
+  } else {
+    $_SESSION['hasil'] = false;
+    $_SESSION['pesan'] = "Gagal Hapus Data Bagian";
+  }
+}
+echo "<meta http-equiv='refresh' content='0;url=?page=bagianread'>";
+
+?>
